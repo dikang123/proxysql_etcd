9361b0b662973d3b2806e5e5eede60bd5b48e2e2
Lei Tian <taylor840326@gmail.com>
2018-01-22 17:41:18 +0800

    update main.go fix update/delete function

--- main.go
+++ main.go
@@ -29,48 +29,29 @@ func main() {
 		log.Fatal(err)
 	}
 	defer cli.Close()
-	log.Println("存储值")
-	if _, err := cli.Put(context.TODO(), "sensors", `{sensor01:{topic:"w_sensor01"}}`); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("获取值")
-	if resp, err := cli.Get(context.TODO(), "sensors"); err != nil {
-		log.Fatal(err)
-	} else {
-		log.Println("resp: ", resp)
-	}
-	// see https://github.com/coreos/etcd/blob/master/clientv3/example_kv_test.go#L220
-	log.Println("事务&超时")
-	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
-	_, err = cli.Txn(ctx).
-		If(clientv3.Compare(clientv3.Value("key"), ">", "abc")). // txn value comparisons are lexical
-		Then(clientv3.OpPut("key", "XYZ")).                      // this runs, since 'xyz' > 'abc'
-		Else(clientv3.OpPut("key", "ABC")).
-		Commit()
-	cancel()
-	if err != nil {
-		log.Fatal(err)
-	}
+
 	// see https://github.com/coreos/etcd/blob/master/clientv3/example_watch_test.go
-	log.Println("监视")
+	log.Println("Running proxysql_etcd as watch mode. the watching path is /database/parauser")
 	rch := cli.Watch(context.Background(), "/database/parauser", clientv3.WithPrefix())
 	for wresp := range rch {
 		for _, ev := range wresp.Events {
 
 			node := strings.Split(string(ev.Kv.Key), "/")
 
-			fmt.Println(node[4])
 			switch node[3] {
 			/*
-				/database/parauser/users/user1/name
-				/database/parauser/users/user1/pass
+				/database/parauser/users/user01 '{"username":"parauser_v2","password":"123456"}'
+				node[1] -> database
+				node[2] -> parauser
+				node[3] -> users
+				node[4] -> user01
 			*/
 			case "users":
 				switch ev.Type {
 				case mvccpb.PUT:
 					switch {
 					case ev.IsCreate():
-						fmt.Printf("%s Create %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
+						fmt.Printf("Create %q : %q\n", ev.Kv.Key, ev.Kv.Value)
 						conn, err := proxysql.NewConn("172.18.10.111", 13306, "admin", "admin")
 						if err != nil {
 							fmt.Println(err)
@@ -84,12 +65,12 @@ func main() {
 							fmt.Println(err)
 						}
 
-						fmt.Println(ev.Kv.Value)
 						var tmpusr proxysql.Users
 						if err := json.Unmarshal(ev.Kv.Value, &tmpusr); err != nil {
 							fmt.Println(err)
 						}
-						fmt.Println(tmpusr)
+						//tmpusr.Username = node[4]
+
 						newuser, err := proxysql.NewUser(tmpusr.Username, tmpusr.Password, 0, tmpusr.Username)
 						if err != nil {
 							fmt.Println(err)
@@ -102,8 +83,7 @@ func main() {
 							fmt.Println(err)
 						}
 					default:
-						fmt.Printf("%s Update %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
-						fmt.Printf("%s Create %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
+						fmt.Printf("Update %q : %q\n", ev.Kv.Key, ev.Kv.Value)
 						conn, err := proxysql.NewConn("172.18.10.111", 13306, "admin", "admin")
 						if err != nil {
 							fmt.Println(err)
@@ -117,12 +97,11 @@ func main() {
 							fmt.Println(err)
 						}
 
-						fmt.Println(ev.Kv.Value)
 						var tmpusr proxysql.Users
 						if err := json.Unmarshal(ev.Kv.Value, &tmpusr); err != nil {
 							fmt.Println(err)
 						}
-						fmt.Println(tmpusr)
+
 						newuser, err := proxysql.NewUser(tmpusr.Username, tmpusr.Password, 0, tmpusr.Username)
 						if err != nil {
 							fmt.Println(err)
@@ -136,8 +115,9 @@ func main() {
 						}
 					}
 				case mvccpb.DELETE:
-					fmt.Printf("%s Default %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
-					fmt.Printf("%s Create %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
+
+					fmt.Printf("Delete %q \n", ev.Kv.Key)
+
 					conn, err := proxysql.NewConn("172.18.10.111", 13306, "admin", "admin")
 					if err != nil {
 						fmt.Println(err)
@@ -151,18 +131,15 @@ func main() {
 						fmt.Println(err)
 					}
 
-					fmt.Println(ev.Kv.Value)
 					var tmpusr proxysql.Users
-					if err := json.Unmarshal(ev.Kv.Value, &tmpusr); err != nil {
-						fmt.Println(err)
-					}
-					fmt.Println(tmpusr)
+					tmpusr.Username = node[4]
+
 					newuser, err := proxysql.NewUser(tmpusr.Username, tmpusr.Password, 0, tmpusr.Username)
 					if err != nil {
 						fmt.Println(err)
 					}
 
-					newuser.SetUserActive(1)
+					//newuser.SetUserActive(1)
 
 					err = newuser.DeleteOneUser(db)
 					if err != nil {
