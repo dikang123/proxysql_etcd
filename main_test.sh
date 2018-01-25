aefe05f46643cd3ea3b14b4eaf7e9f99014cc125
Lei Tian <taylor840326@gmail.com>
2018-01-25 14:44:42 +0800

    add main_test.sh

---
+++ main_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -xeou pipefail
+
+readonly ENDPOINT="172.18.10.136:2379"
+
+export ETCDCTL_API=3
+
+function cmd() {
+	# put/get/del
+	local METHOD=$1
+	# username
+	local USERNAME=$2
+	etcdctl "$METHOD" --endpoints="$ENDPOINT" /database/parauser/users/$USERNAME $3
+}
+
+
+COUNT=0
+while [ 1 ];
+do
+	BASEUSER="parauser"
+	cmd put "$BASEUSER"_"$COUNT" "{'username':'"$BASEUSER"_"$COUNT"','password':'111111'}"
+	COUNT=$(( $COUNT+1 ))
+done
+
