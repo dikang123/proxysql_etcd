4c85856fd0820259eb3bc6648348c2259a52254c
Lei Tian <taylor840326@gmail.com>
2018-02-08 18:44:28 +0800

    add startup debug script start_proxysql_etcd.sh

---
+++ start_proxysql_etcd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ETCD_ADDR="172.18.10.136:2379"
+export ETCD_PREFIX="database"
+export ETCD_SVC="parauser"
+export PROXYSQL_ADDR="172.18.10.136"
+export PROXYSQL_PORT="13306"
+export PROXYSQL_USER="admin"
+export PROXYSQL_PASS="admin"
+
+go run main.go
